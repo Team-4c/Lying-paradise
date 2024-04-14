c9f6a68e192868fa73f7dc422aa6a1d829c8aad7
NITY70 <osincevanton27@gmail.com>
2024-04-14 12:26:25 +0700

    PlayerController
    Добавил PlayerController и временный шаблон игрока

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 1;
+    public float jumpForce = 5;
+
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * speed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(rb.velocity.y) < 0.05f)
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+
+        sr.flipX = movement < 0 ? true : false;
+    }
+}
