252893fecb6b2da6697aa4ea322ae02a156a190c
qqb4me <p_lapko@list.ru>
2024-05-16 19:43:50 +0500

    parallax

---
+++ Assets/Scripts/Parallsx.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallsx : MonoBehaviour
+{
+    [SerializeField, Range(0, 1), Tooltip("0 - неподвижен, 1 - следует за камерой")]
+    private float parallaxStrength;
+
+    private Transform _camera;
+    private Vector3 previousCamPosition;
+
+    private void OnEnable()
+    {
+        _camera = Camera.main.transform;
+        previousCamPosition = _camera.position;
+    }
+
+    private void Update()
+    {
+        if (!_camera) return;
+        var position = _camera.position;
+        Vector3 delta = Vector3.right * (position.x - previousCamPosition.x) * parallaxStrength;
+        transform.Translate(delta);
+        previousCamPosition = position;
+    }
+
+}
+
