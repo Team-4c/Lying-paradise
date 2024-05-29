d8f7285ad61c37ead57d35df9040c91216ae1d36
qqb4me <p_lapko@list.ru>
2024-05-29 14:47:36 +0500

    адекватные коллайдеры + музыка

---
+++ Assets/Scripts/Win.cs
@@ -0,0 +1,17 @@
+﻿using Player;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Win : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+       if (other.GetComponent<CharacterController2D>() != null)
+        {
+            int index = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(index - 1);
+        }
+    }
+}
